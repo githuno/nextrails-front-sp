b316f5498e52c0e0d92ebc15bfc39d0da0cad8d9
githuno <103876809+githuno@users.noreply.github.com>
2024-04-03 07:15:29 +0000

    レイアウトを縦並びに

--- src/app/folio/page.tsx
+++ src/app/folio/page.tsx
@@ -25,50 +25,49 @@ const PortfolioPage = () => {
 			password: 'testpass'
 		},
 	];
-
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2 max-w-screen-xl mx-auto">
-			<h2 className="text-xl mb-6">制作物一例</h2>
-			<div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-				{projects.map((project, index) => (
-					<a href={project.link} target="_blank" rel="noopener noreferrer" key={index} className="flex flex-col">
-						<div className="border rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out flex flex-col h-full gradate">
-							<div className="relative w-full pb-[56.25%]">
-								<Image src={project.img} alt={project.title} layout="fill" objectFit="cover" />
-								<div className="absolute top-0 right-0 p-2">
-									<div className="bg-white rounded-full p-1">
-										<Image src="/open-link.svg" alt="Open link icon" width={24} height={24} />
-									</div>
-								</div>
-							</div>
-							<div className="p-6 flex flex-col justify-between flex-grow">
-								<div>
-									<h3 className="text-2xl mb-4">{project.title}</h3>
-									<p className="mb-4">{project.text}</p>
-									{project.id && project.password && (
-										<div className="overflow-auto">
-										<table className="table-auto">
-											<tbody>
-												<tr>
-													<td className="border px-4 py-2 text-gray-500">user</td>
-													<td className="border px-4 py-2">{project.id}</td>
-												</tr>
-												<tr>
-													<td className="border px-4 py-2 text-gray-500">password</td>
-													<td className="border px-4 py-2">{project.password}</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-									)}
-								</div>
-							</div>
+		  <h2 className="text-xl mb-6">制作物一例</h2>
+		  <div className="flex flex-col items-center gap-4">
+			{projects.map((project, index) => (
+			  <a href={project.link} target="_blank" rel="noopener noreferrer" key={index} className="w-full relative">
+				<div className="border rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out grid grid-cols-3 h-full gradate">
+				  <div className="relative col-span-1">
+					<Image src={project.img} alt={project.title} layout="fill" objectFit="cover" />
+				  </div>
+				  <div className="p-6 flex flex-col justify-between col-span-2">
+					<div>
+					  <h3 className="text-2xl mb-4">{project.title}</h3>
+					  <p className="mb-4">{project.text}</p>
+					  {project.id && project.password && (
+						<div className="overflow-auto">
+						  <table className="table-auto">
+							<tbody>
+							  <tr>
+								<td className="border px-4 py-2 text-gray-500">user</td>
+								<td className="border px-4 py-2">{project.id}</td>
+							  </tr>
+							  <tr>
+								<td className="border px-4 py-2 text-gray-500">password</td>
+								<td className="border px-4 py-2">{project.password}</td>
+							  </tr>
+							</tbody>
+						  </table>
 						</div>
-					</a>
-				))}
-			</div>
+					  )}
+					</div>
+				  </div>
+				</div>
+				<div className="absolute top-0 right-0 p-2">
+				  <div className="bg-white rounded-full p-1">
+					<Image src="/open-link.svg" alt="Open link icon" width={24} height={24} />
+				  </div>
+				</div>
+			  </a>
+			))}
+		  </div>
 		</div>
-	);
+	  );
 };
 
 export default PortfolioPage;
