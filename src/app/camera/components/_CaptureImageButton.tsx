7ac776db7ca04ff07a92f9fdb670f54f1b217e51
githuno <clas.warehouse@gmail.com>
2024-08-15 11:54:57 +0000

    cameraページの実装（録画、撮影、QR、indexdb）

---
+++ src/app/camera/components/_CaptureImageButton.tsx
@@ -0,0 +1,75 @@
+import React, { useRef, useState } from "react";
+import { openDB } from "idb";
+import LoadingSpinner from "./loading";
+
+interface CaptureImageButtonProps {
+  stream: MediaStream | null;
+  setIsScanning: React.Dispatch<React.SetStateAction<boolean>>;
+  onSaved: () => void;
+}
+
+const CaptureImageButton: React.FC<CaptureImageButtonProps> = ({
+  stream,
+  setIsScanning,
+  onSaved,
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const handleCaptureImage = async () => {
+    setIsLoading(true);
+    if (stream && canvasRef.current) {
+      const video = document.createElement("video");
+      video.srcObject = stream;
+      await video.play();
+
+      const canvas = canvasRef.current;
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const context = canvas.getContext("2d");
+      if (context) {
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const blob = await new Promise<Blob | null>((resolve) =>
+          canvas.toBlob((blob) => resolve(blob), "image/png")
+        );
+        if (blob) {
+          await saveImage(blob);
+        }
+      }
+      video.pause();
+      video.srcObject = null;
+      setIsScanning(false);
+    }
+    setIsLoading(false);
+  };
+
+  const saveImage = async (blob: Blob) => {
+    let db = await openDB("images", 3, {
+      upgrade(db) {
+        if (!db.objectStoreNames.contains("photos")) {
+          db.createObjectStore("photos", {
+            keyPath: "id",
+            autoIncrement: true,
+          });
+        }
+      },
+    });
+    let tx = db.transaction("photos", "readwrite");
+    let photos = tx.objectStore("photos");
+    const photoId = new Date().toISOString();
+    const photoData = { id: photoId, blob };
+    await photos.add(photoData);
+    onSaved();
+  };
+
+  return (
+    <div>
+      <button onClick={handleCaptureImage} disabled={isLoading}>
+        {isLoading ? <LoadingSpinner /> : "Capture"}
+      </button>
+      <canvas ref={canvasRef} style={{ display: "none" }} />
+    </div>
+  );
+};
+
+export { CaptureImageButton };
--- src/app/camera/page copy.tsx
+++ src/app/camera/page copy.tsx
@@ -2,12 +2,12 @@
 import React, { useState, useEffect, useRef, Suspense } from "react";
 import { openDB } from "idb";
 import { isMobile } from "react-device-detect";
-import { useToast } from "@/hooks/toast"
+import { LocalVideosList } from "./components/_LocalVideosList";
 
 let mediaRecorder: MediaRecorder;
 let recordedBlobs: Blob[] = [];
 
-const MediaCaptureVeiwer = ({
+const CameraVeiwer = ({
   setStream,
 }: {
   setStream: React.Dispatch<React.SetStateAction<MediaStream | null>>;
@@ -49,7 +49,7 @@ const MediaCaptureVeiwer = ({
             getMediaError(err);
           });
       });
-  }
+  };
 
   // const getAspectRatio = () => {
   //   if (videoRef.current) {
@@ -59,7 +59,8 @@ const MediaCaptureVeiwer = ({
   //   return 0;
   // }
 
-  useEffect(() => { // マウント時に実行 -> getMedia()を実行
+  useEffect(() => {
+    // マウント時に実行 -> getMedia()を実行
     getMedia();
     return () => {
       if (stream) {
@@ -71,7 +72,8 @@ const MediaCaptureVeiwer = ({
     };
   }, []);
 
-  useEffect(() => { // streamが更新されたら実行 -> videoRefにstreamをセット
+  useEffect(() => {
+    // streamが更新されたら実行 -> videoRefにstreamをセット
     if (videoRef.current && stream) {
       videoRef.current.srcObject = stream;
     }
@@ -84,12 +86,12 @@ const MediaCaptureVeiwer = ({
   );
 };
 
-const MediaCaptureButton = ({
+const CaptureVideoButton = ({
   stream,
   setVideoId,
 }: {
   stream: MediaStream | null;
-  setVideoId: React.Dispatch<React.SetStateAction<number | null>>;
+  setVideoId: React.Dispatch<React.SetStateAction<string | null>>;
 }) => {
   const [recording, setRecording] = useState(false);
 
@@ -119,16 +121,24 @@ const MediaCaptureButton = ({
   };
 
   const saveRecording = async (blob: Blob) => {
-    let db = await openDB("recordings", 2, {
+    let db = await openDB("recordings", 3, {
       upgrade(db) {
-        db.createObjectStore("videos", { keyPath: "id", autoIncrement: true});
+        if (!db.objectStoreNames.contains("videos")) {
+          db.createObjectStore("videos", {
+            keyPath: "id",
+            autoIncrement: true,
+          });
+        }
       },
     });
     let tx = db.transaction("videos", "readwrite");
     let videos = tx.objectStore("videos");
-    await videos.clear(); // addする前にすべてのデータを削除する
-    const video = await videos.add(blob, isMobile? Math.random() : undefined); // mobileとPCで成功条件が違った。2024/01/14
-    setVideoId(Number(video));
+    // await videos.clear(); // addする前にすべてのデータを削除する
+    // const video = await videos.add(blob, isMobile? Math.random() : undefined); // mobileとPCで成功条件が違った。2024/01/14
+    const videoId = new Date().toISOString(); // 録画が作成された日時データの文字列
+    const videoData = { id: videoId, blob }; // 直接Blobを保存
+    await videos.add(videoData); // 第二引数を削除
+    setVideoId(videoId);
   };
 
   return (
@@ -140,48 +150,10 @@ const MediaCaptureButton = ({
   );
 };
 
-const VideoPlayButton = ({
-  videoId,
-  setRecordedVideo,
-}: {
-  videoId: number | null;
-  setRecordedVideo: React.Dispatch<React.SetStateAction<string | null>>;
-}) => {
-  const playRecordedVideo = async () => {
-    const db = await openDB("recordings", 2);
-    const tx = db.transaction("videos", "readonly");
-
-    // オブジェクトストアの中身を一覧で取得
-    const allVideos = await tx.objectStore("videos").getAll(); // debug
-    console.log("allVideos", allVideos); // debug -> OK!!
-
-    const videoBlob = await tx
-      .objectStore("videos")
-      .get(videoId as IDBKeyRange | IDBValidKey);
-
-    if (videoBlob) {
-      const videoBlobUrl = URL.createObjectURL(videoBlob);
-      setRecordedVideo(videoBlobUrl);
-
-      // コンポーネントが unmount された後も URL を解放するための処理
-      return () => {
-        URL.revokeObjectURL(videoBlobUrl);
-      };
-    }
-  };
-
-  return (
-    <div>
-      <button onClick={playRecordedVideo}>Play Recorded Video</button>
-    </div>
-  );
-};
-
 export default function MediaCapture() {
   const [stream, setStream] = useState<MediaStream | null>(null);
-  const [recordedVideo, setRecordedVideo] = useState<string | null>(null);
   const [isClient, setIsClient] = useState(false);
-  const [videoId, setVideoId] = useState<number | null>(null);
+  const [videoId, setVideoId] = useState<string | null>(null);
   // サーバーサイドとクライアントサイドで同じメッセージを表示する
   const loadingMessage = "Loading...";
   useEffect(() => {
@@ -195,27 +167,25 @@ export default function MediaCapture() {
   console.log("MediaCapture実行"); // debug
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div id="media-container" className="min-h-7/10 bg-gray-200 rounded-lg shadow-lg">
-        <MediaCaptureVeiwer
-          setStream={setStream}
-        />
+      <div
+        id="media-container"
+        className="min-h-7/10 bg-gray-200 rounded-lg shadow-lg"
+      >
+        <CameraVeiwer setStream={setStream} />
       </div>
       <div className="flex items-center justify-center w-24 h-24 bg-blue-500 rounded-full shadow-lg">
-        <MediaCaptureButton stream={stream} setVideoId={setVideoId} />
+        <CaptureVideoButton stream={stream} setVideoId={setVideoId} />
       </div>
-      {recordedVideo && <video controls src={recordedVideo} />}
-      <VideoPlayButton videoId={videoId} setRecordedVideo={setRecordedVideo} />
+      <LocalVideosList />
     </main>
   );
 }
 
-// indexedDBのデータを一覧化する（getIndexedDbボタン）-> (play)(delete)
 // uploadボタンを作成する->create3D（uploadボタン<createObject && create3D>）
 // CloudDB（Objects）のデータを一覧化する（getObjectsボタン） -> (play)(delete)
 
 // -- CloudDB（images）のデータを一覧化する
 // -- CloudDB（image）のデータを削除する
-
 
 // // Error Boundary コンポーネント
 // type Props = {
