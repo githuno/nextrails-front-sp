e13485716367da0904bef08fba0b05f2b6d30eea
githuno <clas.warehouse@gmail.com>
2025-01-17 13:34:02 +0000

    serviceWorker部分をコメントアウト

--- src/components/camera/index.tsx
+++ src/components/camera/index.tsx
@@ -3,54 +3,54 @@ import { CameraPreview } from "./sensor/CameraPreview";
 import { Controller } from "./controls";
 import { Showcase } from "./showcase";
 import { CameraProvider, useCameraContext } from "./CameraContext";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 const CameraContent: React.FC = () => {
-  const { imageset } = useCameraContext();
-  useEffect(() => {
-    if ("serviceWorker" in navigator && "PushManager" in window) {
-      console.log("Service Worker and Push is supported");
+  // const { imageset } = useCameraContext();
+  // useEffect(() => {
+  //   if ("serviceWorker" in navigator && "PushManager" in window) {
+  //     console.log("Service Worker and Push is supported");
 
-      navigator.serviceWorker
-        .register("/sw.js", {
-          scope: "/",
-          updateViaCache: "none",
-          type: "module",
-        })
-        .then((reg) => {
-          console.log("⚡️Service Worker registered", reg);
-          const sw = reg.installing || reg.waiting || reg.active;
-          console.log("sw-state:", sw?.state);
-          sw?.addEventListener("statechange", () => {
-            console.log("change sw-state:", sw?.state);
-          });
-        })
-        .catch((err) => {
-          console.log("Service Worker registration failed: ", err);
-        });
+  //     navigator.serviceWorker
+  //       .register("/sw.js", {
+  //         scope: "/",
+  //         updateViaCache: "none",
+  //         type: "module",
+  //       })
+  //       .then((reg) => {
+  //         console.log("⚡️Service Worker registered", reg);
+  //         const sw = reg.installing || reg.waiting || reg.active;
+  //         console.log("sw-state:", sw?.state);
+  //         sw?.addEventListener("statechange", () => {
+  //           console.log("change sw-state:", sw?.state);
+  //         });
+  //       })
+  //       .catch((err) => {
+  //         console.log("Service Worker registration failed: ", err);
+  //       });
 
-      navigator.serviceWorker.addEventListener("message", (event) => {
-        if (event.data && event.data.type === "ALERT_IMAGESET_FILES") {
-          alert(event.data.message);
-        }
-      });
-    }
-  }, []);
+  //     navigator.serviceWorker.addEventListener("message", (event) => {
+  //       if (event.data && event.data.type === "ALERT_IMAGESET_FILES") {
+  //         alert(event.data.message);
+  //       }
+  //     });
+  //   }
+  // }, []);
 
-  const handleButtonClick = () => {
-    if (navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller.postMessage({
-        type: "CHECK_IMAGESET_FILES",
-        imageset: imageset,
-      });
-    } else {
-      alert("Service Worker not ready");
-    }
-  };
+  // const handleButtonClick = () => {
+  //   if (navigator.serviceWorker.controller) {
+  //     navigator.serviceWorker.controller.postMessage({
+  //       type: "CHECK_IMAGESET_FILES",
+  //       imageset: imageset,
+  //     });
+  //   } else {
+  //     alert("Service Worker not ready");
+  //   }
+  // };
 
   return (
     <>
-      <button
+      {/* <button
         id="callHelloButton"
         onClick={() => {
           handleButtonClick();
@@ -59,7 +59,7 @@ const CameraContent: React.FC = () => {
         className="fixed top-1 z-50 right-1 p-2 bg-blue-500 text-white rounded-md"
       >
         Call Hello Function
-      </button>
+      </button> */}
       <div className="flex h-full w-full justify-center">
         <CameraPreview onQRCodeScanned={(data) => alert(data)} />
       </div>
