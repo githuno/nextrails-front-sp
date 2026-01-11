5cc0d9e48b0cdb158c5bc1f4c3d62f76c3d15986
githuno <clas.warehouse@gmail.com>
2026-01-11 12:26:57 +0000

    PG用UI/ npm install @electric-sql/pglite-repl

--- src/app/(frontend)/(public)/_globalTools/FAB.tsx
+++ src/app/(frontend)/(public)/_globalTools/FAB.tsx
@@ -1,19 +1,28 @@
 "use client"
 
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import FloatingActionButton from "./_components/FloatingActionButton"
 import { CameraIcon } from "./_components/Icons"
 import { useSessionSync } from "./_hooks/useSessionSync"
 import { useToolActionStore } from "./_hooks/useToolActionStore"
 import CameraModal from "./camera/Modal.Camera"
 
+// PGlite関連のimportは開発モード時のみ行う ------------------
+import { Repl } from "@electric-sql/pglite-repl"
+import { Modal } from "./_components/atoms/Modal"
+import { usePgliteStore } from "./_hooks/db/usePgliteStore"
+// ---------------------------------------------------------
+
 const FAB: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ className }) => {
   useSessionSync() // URLとStateのセッション同期を有効化
-
   const { isReady, isCameraOpen, isWebViewOpen, webUrl, handleScan, addFiles, setCameraOpen, closeWebView } =
     useToolActionStore()
-
   const fileInputRef = useRef<HTMLInputElement>(null)
+
+  // PGlite関連の状態管理 ------------------------------
+  const { db } = usePgliteStore()
+  const [isPgliteOpen, setPgliteOpen] = useState(false)
+  // --------------------------------------------------
 
   const handleSelect = () => {
     fileInputRef.current?.click()
@@ -24,6 +33,7 @@ const FAB: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ classNam
     { id: 2, label: "Text", onClick: () => alert("Text Component") },
     { id: 3, label: "Voice", onClick: () => alert("Voice Component") },
     { id: 4, label: "File", onClick: handleSelect },
+    ...(process.env.NODE_ENV === "development" ? [{ id: 5, label: "PGLite", onClick: () => setPgliteOpen(true) }] : []),
   ]
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,7 +50,6 @@ const FAB: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ classNam
     <div className="pointer-events-none fixed top-0 z-50 h-svh w-svw overflow-hidden">
       {/* Hidden file input for gallery selection */}
       <input type="file" ref={fileInputRef} onChange={handleFileChange} accept="image/*" multiple className="hidden" />
-
       <FloatingActionButton.Simple items={fabItems} className={className} />
 
       <CameraModal
@@ -52,6 +61,14 @@ const FAB: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ classNam
         isWebViewOpen={isWebViewOpen}
         onWebViewClose={closeWebView}
       />
+
+      <Modal
+        isOpen={isPgliteOpen}
+        onClose={() => setPgliteOpen(false)}
+        className="fixed top-[10vh] z-50 h-[80vh] w-[90vw] bg-white"
+      >
+        <Repl pg={db?.$client} theme="dark" border={true} />
+      </Modal>
     </div>
   )
 }
