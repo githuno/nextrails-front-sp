574666d3f71c71cfc555ec1e61866c03cfa77637
githuno <clas.warehouse@gmail.com>
2026-01-30 17:57:36 +0000

    npm install react-markdown remark-gfm &&  npm install -D @tailwindcss/typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import typography from "@tailwindcss/typography"
 import type { Config } from "tailwindcss"
 
 const config: Config = {
@@ -6,6 +7,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [typography],
 }
 
 export default config
